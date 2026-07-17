4fd7b5355bd22c169accb1695e5f0abb4f81395c
moreteekay <teekaythewriter@gmail.com>
2026-07-17 16:35:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 window.ATLAS_CONFIG = {
-  placesUrl: "",
-  contentUrl: "",
-  photosUrl: "",
+  placesUrl:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQymoQvG1l4VcFhJ5fFtSDSb-FKgnX1nD72oJgabtMqZWsxYRmNhViBCzx-9Iic5ylNinjcEUli4RKI/pub?gid=1819290401&single=true&output=csv",
+
+  contentUrl:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQymoQvG1l4VcFhJ5fFtSDSb-FKgnX1nD72oJgabtMqZWsxYRmNhViBCzx-9Iic5ylNinjcEUli4RKI/pub?gid=294750024&single=true&output=csv",
+
+  photosUrl:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQymoQvG1l4VcFhJ5fFtSDSb-FKgnX1nD72oJgabtMqZWsxYRmNhViBCzx-9Iic5ylNinjcEUli4RKI/pub?gid=1231707046&single=true&output=csv",
+
   localFallback: true
 };
