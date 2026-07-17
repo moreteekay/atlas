a9da2172f3b16cfa93c37227fab7abc527ea0c48
moreteekay <teekaythewriter@gmail.com>
2026-07-17 22:55:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,3 @@
-window.ATLAS_CONFIG = {
-  placesUrl:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQymoQvG1l4VcFhJ5fFtSDSb-FKgnX1nD72oJgabtMqZWsxYRmNhViBCzx-9Iic5ylNinjcEUli4RKI/pub?gid=1819290401&single=true&output=csv",
-
-  contentUrl:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQymoQvG1l4VcFhJ5fFtSDSb-FKgnX1nD72oJgabtMqZWsxYRmNhViBCzx-9Iic5ylNinjcEUli4RKI/pub?gid=294750024&single=true&output=csv",
-
-  photosUrl:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQymoQvG1l4VcFhJ5fFtSDSb-FKgnX1nD72oJgabtMqZWsxYRmNhViBCzx-9Iic5ylNinjcEUli4RKI/pub?gid=1231707046&single=true&output=csv",
-
-  localFallback: true
+window.ATLAS_CONFIG={
+  apiUrl:"https://script.google.com/macros/s/AKfycbzmWrgEKumcz8ZLFypmn5--BUxgMFiz3j-FY1nwQ42fY_q1YZPu3Nf2re9qrgcOMjN9xQ/exec"
 };
